549c5dc2a6ded49f64ca88ed02cdc4725c362123
aitziberE <eskizabel.aitziber@gmail.com>
2024-06-30 16:23:44 +0200

    orders tested. orderProductController and routes deleted

--- routes/ordersproducts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const OrderProductController = require('../controllers/OrderProductController')
-
-
-router.get('/', OrderProductController.getAll)
-
-
-module.exports = router
